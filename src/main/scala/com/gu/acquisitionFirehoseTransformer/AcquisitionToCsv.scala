bf1ea5d67f99c8efe139adf05f69c95e09030b7d
tomrf1 <tom.forbes@theguardian.com>
2018-12-10 13:01:07 +0000

    Implement lambda

---
+++ src/main/scala/com/gu/acquisitionFirehoseTransformer/AcquisitionToCsv.scala
@@ -0,0 +1,15 @@
+package com.gu.acquisitionFirehoseTransformer
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import ophan.thrift.event.Acquisition
+
+object AcquisitionToCsv {
+  private val formatter = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss")
+
+  def apply(acquisition: Acquisition, timestamp: Long): String = {
+    val dateTime = formatter.format(new Date(timestamp))
+    s"${acquisition.paymentFrequency},${acquisition.countryCode},${acquisition.amount},${acquisition.currency},$dateTime"
+  }
+}
