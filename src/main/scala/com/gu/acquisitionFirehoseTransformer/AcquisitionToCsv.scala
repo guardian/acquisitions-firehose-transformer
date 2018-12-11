2a8ba5bc3aa64ca2338000e9d7b617ae9e3a4a29
tomrf1 <tom.forbes@theguardian.com>
2018-12-11 10:17:19 +0000

    cleaner

--- src/main/scala/com/gu/acquisitionFirehoseTransformer/AcquisitionToCsv.scala
+++ src/main/scala/com/gu/acquisitionFirehoseTransformer/AcquisitionToCsv.scala
@@ -9,7 +9,12 @@ object AcquisitionToCsv {
   private val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
   def apply(acquisition: Acquisition, timestamp: Long): String = {
+    val frequency = acquisition.paymentFrequency
+    val countryCode = acquisition.countryCode.getOrElse("")
+    val amount = acquisition.amount
+    val currency = acquisition.currency
     val dateTime = formatter.format(new Date(timestamp))
-    s"${acquisition.paymentFrequency},${acquisition.countryCode.getOrElse("")},${acquisition.amount},${acquisition.currency},$dateTime"
+
+    s"$frequency,$countryCode,$amount,$currency,$dateTime"
   }
 }
