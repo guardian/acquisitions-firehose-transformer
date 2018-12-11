9a5760f69d5641846b79778feb013987afba365b
tomrf1 <tom.forbes@theguardian.com>
2018-12-11 10:05:27 +0000

    fix formatting

--- src/main/scala/com/gu/acquisitionFirehoseTransformer/AcquisitionToCsv.scala
+++ src/main/scala/com/gu/acquisitionFirehoseTransformer/AcquisitionToCsv.scala
@@ -6,10 +6,10 @@ import java.util.Date
 import ophan.thrift.event.Acquisition
 
 object AcquisitionToCsv {
-  private val formatter = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss")
+  private val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
   def apply(acquisition: Acquisition, timestamp: Long): String = {
     val dateTime = formatter.format(new Date(timestamp))
-    s"${acquisition.paymentFrequency},${acquisition.countryCode},${acquisition.amount},${acquisition.currency},$dateTime"
+    s"${acquisition.paymentFrequency},${acquisition.countryCode.getOrElse("")},${acquisition.amount},${acquisition.currency},$dateTime"
   }
 }
